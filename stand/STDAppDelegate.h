d73812e0e8bbc0202911c544df39e0e587690741
Adithya Ravikumar <adithyaravikumar@yahoo.com>
2014-04-25 17:11:52 -0400

    Initial Commit

---
+++ stand/STDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STDAppDelegate.h
+//  stand
+//
+//  Created by Adithya Ravikumar on 4/25/14.
+//  Copyright (c) 2014 stand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ stand/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  stand
+//
+//  Created by Adithya Ravikumar on 4/25/14.
+//  Copyright (c) 2014 stand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STDAppDelegate class]));
+    }
+}
